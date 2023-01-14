0898cd0fe2497a40cb40a91fc9312f73f22a3898
Marcos Danilo <marcosdanilo94@gmail.com>
2023-01-14 14:24:13 -0300

    feat: create method initialSetup
    the method insert the pieces on the chessBoard on their initial
    positions when the chessMatch initiate.

--- src/model/entities/ChessLayer/ChessMatch.java
+++ src/model/entities/ChessLayer/ChessMatch.java
@@ -1,7 +1,10 @@
 package model.entities.ChessLayer;
 
 import model.entities.BoardLayer.Board;
+import model.entities.BoardLayer.Position;
 import model.entities.ChessLayer.enums.Color;
+import model.entities.ChessLayer.pieces.King;
+import model.entities.ChessLayer.pieces.Rook;
 
 public class ChessMatch {
   private Board board;
@@ -14,6 +17,7 @@ public class ChessMatch {
 
   public ChessMatch() {
     board = new Board(8, 8);
+    initialSetup();
   }
 
   public ChessPiece[][] getPieces() {
@@ -27,5 +31,11 @@ public class ChessMatch {
     }
 
     return matrix;
+  }
+
+  private void initialSetup() {
+    board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+    board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+    board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
   }
 }
