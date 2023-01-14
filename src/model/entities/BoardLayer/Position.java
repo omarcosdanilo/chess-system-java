7f6566d45f13ecb3617d9e5c63ca3d261e159535
Marcos Danilo <marcosdanilo94@gmail.com>
2023-01-14 13:06:40 -0300

    feat: create the class Position

---
+++ src/model/entities/BoardLayer/Position.java
@@ -0,0 +1,31 @@
+package model.entities.BoardLayer;
+
+public class Position {
+  private Integer row;
+  private Integer column;
+
+  public Position() {}
+
+  public Position(Integer row, Integer column) {
+    this.row = row;
+    this.column = column;
+  }
+
+  public Integer getRow() {
+    return row;
+  }
+
+  public Integer getColumn() {
+    return column;
+  }
+
+  public void setValues(int row, int column) {
+    this.row = row;
+    this.column = column;
+  }
+
+  @Override
+  public String toString() {
+    return row + ", " + column;
+  }
+}
