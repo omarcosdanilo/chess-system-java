6491d27fc4ad41b3d56dc3ed5f4674182cf93f5b
Marcos Danilo <marcosdanilo94@gmail.com>
2023-01-14 17:28:14 -0300

    refactor: change how a new piece is pisiotioned on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,12 @@
 package application;
+
+import model.entities.ChessLayer.ChessMatch;
+
 public class Program {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {   
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.getPieces());
+        
     }
 }
