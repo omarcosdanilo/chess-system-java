d5e92db9c2a1010b70d8103a6964a6820bf1f960
Marcos Danilo <marcosdanilo94@gmail.com>
2023-01-16 20:43:57 -0300

    feat: create method testChecMate.
    the method verify if the king is in check mate.

--- src/model/entities/ChessLayer/ChessMatch.java
+++ src/model/entities/ChessLayer/ChessMatch.java
@@ -33,6 +33,10 @@ public class ChessMatch {
   public boolean getCheck() {
     return check;
   }
+
+  public boolean getCheckMate() {
+    return checkMate;
+  }
   
   public int getTurn() {
     return turn;
@@ -175,19 +179,44 @@ public class ChessMatch {
     return false;
   }
 
-  private void initialSetup() {
-    placeNewPiece('c', 1, new Rook(board, Color.WHITE));
-    placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-    placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-    placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-    placeNewPiece('e', 1, new Rook(board, Color.WHITE));
-    placeNewPiece('d', 1, new King(board, Color.WHITE));
+  private boolean testCheckMate(Color color) {
+    if (!testCheck(color)) {
+      return false;
+    }
 
-    placeNewPiece('c', 7, new Rook(board, Color.BLACK));
-    placeNewPiece('c', 8, new Rook(board, Color.BLACK));
-    placeNewPiece('d', 7, new Rook(board, Color.BLACK));
-    placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-    placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-    placeNewPiece('d', 8, new King(board, Color.BLACK));
+    List<Piece> list = piecesOnTheBoard.stream()
+    .filter(x -> ((ChessPiece)x).getColor() == color)
+    .collect(Collectors.toList());
+
+    for (Piece piece : list) {
+      boolean[][] mat = piece.possibleMoves();
+
+      for (int i = 0; i < board.getRows(); i++) {
+        for (int j = 0; j < board.getColumns(); j++) {
+          if (mat[i][j]) {
+            Position source = ((ChessPiece)piece).getChessPosition().toPosition();
+            Position target = new Position(i, j);
+            Piece capturedPiece = makeMove(source, target);
+            boolean testCheck = testCheck(color);
+            undoMove(source, target, capturedPiece);
+
+            if (!testCheck) {
+              return false;
+            }
+          }
+        }
+      }
+    }
+
+    return true;
+  }
+
+  private void initialSetup() {
+   placeNewPiece('h', 7, new Rook(board, Color.WHITE));
+   placeNewPiece('d', 1, new Rook(board, Color.WHITE));
+   placeNewPiece('e', 1, new King(board, Color.WHITE));
+
+   placeNewPiece('b', 8, new Rook(board, Color.BLACK));
+   placeNewPiece('a', 8, new King(board, Color.BLACK));
   }
 }
