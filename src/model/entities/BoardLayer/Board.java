30c7f2d9a67bbd14d551ceb324c832a80bb851d6
Marcos Danilo <marcosdanilo94@gmail.com>
2023-01-16 15:13:51 -0300

    feat: cria o método removePiece.
    O método removePiece remove uma peça do tabuleiro de xadrez.

--- src/model/entities/BoardLayer/Board.java
+++ src/model/entities/BoardLayer/Board.java
@@ -64,4 +64,22 @@ public class Board {
     }
     return piece(position) != null;
   }
+
+  public Piece removePiece(Position position) throws BoardException {
+    if (!positionExists(position)) {
+      throw new BoardException("Position not on the board");
+    }
+
+    if (piece(position) == null) {
+      return null;
+    }
+
+    Piece aux = piece(position);
+
+    aux.position = null;
+
+    pieces[position.getRow()][position.getColumn()] = null;
+
+    return aux;
+  }
 }
