f999d850e6124a46ed6c796cc6ad27615b5d3e82
Marcos Danilo <marcosdanilo94@gmail.com>
2023-01-16 19:03:14 -0300

    refactor: call the printCapturedPieces in method printMatch

--- src/application/UI.java
+++ src/application/UI.java
@@ -73,8 +73,10 @@ public class UI {
     System.out.println("  a b c d e f g h");
   }
 
-  public static void printMatch(ChessMatch chessMatch) {
+  public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
     printBoard(chessMatch.getPieces());
+    System.out.println();
+    printCapturedPieces(captured);
     System.out.println();
     System.out.println("Turn: " + chessMatch.getTurn());
     System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
