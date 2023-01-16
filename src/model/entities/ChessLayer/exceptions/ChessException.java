ff2c68d15d5cd6e8f047795cf6cff98253dde65c
Marcos Danilo <marcosdanilo94@gmail.com>
2023-01-16 15:34:15 -0300

    refactor: change the inheritance of ChessException.
    this change makes easier to catch a ChessException or a BoardException because the method validateSourcePosition uses the method thereIsAPiece from the board and the method thereIsAPiece throws a BoardException.

--- src/model/entities/ChessLayer/exceptions/ChessException.java
+++ src/model/entities/ChessLayer/exceptions/ChessException.java
@@ -1,6 +1,8 @@
 package model.entities.ChessLayer.exceptions;
 
-public class ChessException extends RuntimeException {
+import model.entities.BoardLayer.exceptions.BoardException;
+
+public class ChessException extends BoardException {
   public ChessException(String msg) {
     super(msg);
   }
