f4026ead5656a2359434ed98ed4f8f4e9921683d
Marcos Danilo <marcosdanilo94@gmail.com>
2023-01-16 20:49:48 -0300

    refactor: add a verification to check if the game is in checkmate.

--- src/application/UI.java
+++ src/application/UI.java
@@ -79,7 +79,14 @@ public class UI {
     printCapturedPieces(captured);
     System.out.println();
     System.out.println("Turn: " + chessMatch.getTurn());
-    System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+
+    if (!chessMatch.getCheckMate()) {
+      System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+    }
+    else {
+      System.out.println("CHECKMATE!");
+      System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+    }
 
     if (chessMatch.getCheck()) {
       System.out.println("CHECK!");
